Improve GPU memory efficiency and fix rare backprop NaN issue.

run_nerf.py
@@ -28,7 +28,7 @@ from run_nerf_helpers import (add_1d_imgs_to_tensorboard, append_to_log_file, cr
                               get_sw_n_sampled, get_sw_rays, get_embedder, get_rays,
                               get_sw_sampling_prob_dist_modifier,
                               intrinsics_params_from_intrinsics_matrix, img2mse, initialize_sw_kf_loss,
-                              load_data, log_depth_loss_meters_multiplier_function,
+                              load_data, load_data_and_create_keyframes, log_depth_loss_meters_multiplier_function,
                               log_depth_loss_iters_multiplier_function,
                               mse2psnr, NeRF, ndc_rays, pad_imgs, pad_sections, render,
                               render_and_compute_loss, sample_pdf, sample_skf_rays, sample_sw_rays,
@@ -501,7 +501,7 @@ def config_parser():
                         'gaussian positional encoding matrix, B.')
     parser.add_argument('--no_depth_measurements', action='store_true', help='Set to ignore sensor '
                         'depth measurements. Disables direct learning of depth.')
-    parser.add_argument('--depth_loss_iters_diminish_point', type=int, default=10000, help='Used to tune '
+    parser.add_argument('--depth_loss_iters_diminish_point', type=int, default=5000, help='Used to tune '
                         'how quickly the depth loss will be diminished. After the specified number '
                         'of iterations, the exponentially decaying function that determines how much'
                         'the depth loss is diminished will equal 0.01 (ie, 99% of the depth loss '
@@ -521,26 +521,10 @@ def train() -> None:
     vis_dpath.mkdir(parents=True, exist_ok=True)
     tensorboard = SummaryWriter(log_dpath, flush_secs=10)
 
-    # Load data from specified dataset.
-    (rgb_imgs, depth_imgs, hwf, img_height, img_width, focal, initial_intrinsics_matrix, initial_poses,
-     render_poses, train_idxs, test_idxs, val_idxs, near, far) = load_data(args, gpu_if_available)
-    test_rgb_imgs = rgb_imgs[test_idxs]
-    test_depth_imgs = depth_imgs[test_idxs]
-    test_poses = initial_poses[test_idxs]
-    # Log the test images to tensorboard.
-    tensorboard.add_images('test/rgb/groundtruth',
-                           pad_imgs(test_rgb_imgs, white_rgb, padding_width=2),
-                           global_step=1, dataformats='NHWC')
-    add_1d_imgs_to_tensorboard(test_depth_imgs, black_rgb, white_rgb, tensorboard, 'test/depth/groundtruth',
-                               1, cpu, white_rgb, 2)
-    val_idx = val_idxs[len(val_idxs) // 2]
-    val_rgb_img = rgb_imgs[val_idx]
-    val_depth_img = depth_imgs[val_idx]
-    val_pose = initial_poses[val_idx, :3, :4].to(gpu_if_available)
-
-    kf_rgb_imgs, kf_depth_imgs, kf_initial_poses, kf_idxs, n_kfs = \
-        create_keyframes(rgb_imgs, depth_imgs, initial_poses, train_idxs, args.keyframe_creation_strategy,
-                         args.every_Nth)
+    (hwf, img_height, img_width, focal, initial_intrinsics_matrix, initial_poses, render_poses,
+     near, far, val_rgb_img, val_depth_img, val_pose,
+     kf_rgb_imgs, kf_depth_imgs, kf_initial_poses, kf_idxs, n_kfs) = \
+        load_data_and_create_keyframes(args, gpu_if_available, tensorboard, cpu)
 
     # Create log dir and copy the config file
     basedir = args.basedir
@@ -669,9 +653,9 @@ def train() -> None:
                                   args.chunk, optimizer, do_active_sampling, tensorboard,
                                   cpu, gpu_if_available, verbose=verbose)
     sw_total_n_sampled = torch.zeros(dims_kf_sw, dtype=torch.int64)
-    tensorboard.add_images('train/keyframes',
-                           pad_sections(kf_rgb_imgs, dims_kf_pw, white_rgb, padding_width=2),
-                           global_step=1, dataformats='NHWC')
+    # tensorboard.add_images('train/keyframes',
+    #                        pad_sections(kf_rgb_imgs, dims_kf_pw, white_rgb, padding_width=2),
+    #                        global_step=1, dataformats='NHWC')
     sw_sampling_prob_dist_modifier = \
         get_sw_sampling_prob_dist_modifier(kf_rgb_imgs, grid_size,
                                            args.sw_sampling_prob_dist_modifier_strategy,
@@ -693,6 +677,8 @@ def train() -> None:
     t_training_start = perf_counter()
 
     already_logged_val_gt = False
+
+    showUtilization()
 
     start_iter_idx += 1
     open3d_vis_count += 1
@@ -831,7 +817,6 @@ def train() -> None:
             if render_kwargs_train['network_fine'] is not None:
                 save_dict['network_fine_state_dict'] = render_kwargs_train['network_fine'].state_dict()
             torch.save(save_dict, path)
-            set_trace()
             print('Saved checkpoints at', path)
             t_prev_weights_log = t_train_iter_start
 
@@ -846,22 +831,22 @@ def train() -> None:
             imageio.mimwrite(moviebase + 'rgb.mp4', to8b(rgbs), fps=30, quality=8)
             imageio.mimwrite(moviebase + 'disp.mp4', to8b(disps / np.max(disps)), fps=30, quality=8)
 
-        if log_test:
-            testsavedir = os.path.join(basedir, expname, f'testset_{train_iter_idx:06d}')
-            os.makedirs(testsavedir, exist_ok=True)
-            with torch.no_grad():
-                test_rendered_rgbs_np, _ = render_path(
-                    test_poses.to(gpu_if_available), hwf, intrinsics_matrix, args.chunk,
-                    render_kwargs_test,
-                    gt_imgs=test_rgb_imgs, savedir=testsavedir)
-                test_rendered_rgbs = torch.from_numpy(test_rendered_rgbs_np)
-                test_loss = img2mse(test_rendered_rgbs, test_rgb_imgs.to(cpu))
-                test_psnr = mse2psnr(test_loss)
+#         if log_test:
+#             testsavedir = os.path.join(basedir, expname, f'testset_{train_iter_idx:06d}')
+#             os.makedirs(testsavedir, exist_ok=True)
+#             with torch.no_grad():
+#                 test_rendered_rgbs_np, _ = render_path(
+#                     test_poses.to(gpu_if_available), hwf, intrinsics_matrix, args.chunk,
+#                     render_kwargs_test,
+#                     gt_imgs=test_rgb_imgs, savedir=testsavedir)
+#                 test_rendered_rgbs = torch.from_numpy(test_rendered_rgbs_np)
+#                 test_loss = img2mse(test_rendered_rgbs, test_rgb_imgs.to(cpu))
+#                 test_psnr = mse2psnr(test_loss)
 
-            tensorboard.add_images('test/rgb/estimate', pad_imgs(test_rendered_rgbs, white_rgb, 2),
-                                   train_iter_idx, dataformats='NHWC')
-            tensorboard.add_scalar('test/loss', test_loss, train_iter_idx)
-            tensorboard.add_scalar('test/psnr', test_psnr, train_iter_idx)
+#             tensorboard.add_images('test/rgb/estimate', pad_imgs(test_rendered_rgbs, white_rgb, 2),
+#                                    train_iter_idx, dataformats='NHWC')
+#             tensorboard.add_scalar('test/loss', test_loss, train_iter_idx)
+#             tensorboard.add_scalar('test/psnr', test_psnr, train_iter_idx)
 
         if log_train_scalars:
             with torch.no_grad():

run_nerf_helpers.py
@@ -520,6 +520,7 @@ def render_rays(ray_batch: torch.Tensor, network_fn: NeRF, network_query_fn: Cal
         ret['acc0'] = acc_map_0
         ret['z_std'] = torch.std(z_samples, dim=-1, unbiased=False)  # [N_rays]
 
+    found_nan = False
     for k in ret:
         is_disp = k[:4] == 'disp'
         is_nan = torch.isnan(ret[k])
@@ -527,10 +528,12 @@ def render_rays(ray_batch: torch.Tensor, network_fn: NeRF, network_query_fn: Cal
             n_nan = torch.sum(is_nan)
             if is_disp:
                 print(f'Found {n_nan} NaN values in {k}. Replacing with inf.')
-                torch.nan_to_num_(ret[k], nan=torch.inf)
+                # torch.nan_to_num_(ret[k], nan=torch.inf)
+                found_nan = True
             else:
                 print(f'Found {n_nan} NaN values in {k}. Replacing with zeros.')
-                torch.nan_to_num_(ret[k], nan=0)
+                # torch.nan_to_num_(ret[k], nan=0)
+                found_nan = True
 
         if is_disp:
             continue
@@ -539,7 +542,11 @@ def render_rays(ray_batch: torch.Tensor, network_fn: NeRF, network_query_fn: Cal
         if is_inf.any():
             n_inf = torch.sum(is_inf)
             print(f'Found {n_inf} inf values in {k}. Replacing with zeros.')
-            torch.nan_to_num_(ret[k], posinf=0, neginf=0)
+            # torch.nan_to_num_(ret[k], posinf=0, neginf=0)
+            found_nan = True
+
+    if found_nan:
+        set_trace()
 
     return ret
 
@@ -592,7 +599,10 @@ def raw2outputs(raw, z_vals, rays_d, gpu_if_available, raw_noise_std=0, white_bk
     weights_sum = weights.sum(-1)
     depth_map = (weights * z_vals).sum(-1)
     # NOTE: Is this nan_to_num still needed?
-    disp_map = (1 / (depth_map / weights_sum).clamp(min=1e-10)).nan_to_num(nan=torch.inf)
+    # disp_map = (1 / (depth_map / weights_sum).clamp(min=1e-10)).nan_to_num(nan=torch.inf)
+    disp_map = 1 / (depth_map / weights_sum).clamp(min=1e-10)
+    if disp_map.isnan().any():
+        set_trace()
     acc_map = weights_sum
 
     if white_bkgd:
@@ -604,7 +614,7 @@ def raw2outputs(raw, z_vals, rays_d, gpu_if_available, raw_noise_std=0, white_bk
     return rgb_map, disp_map, acc_map, weights, depth_map
 
 
-def load_data(args: Namespace, gpu_if_available: torch.device
+def load_data(args: Namespace, gpu_if_available: torch.device, cpu
               ) -> Tuple[torch.Tensor, torch.Tensor, List[int], int, int, float, torch.Tensor,
                          torch.Tensor, torch.Tensor, List[int], List[int], List[int], float, float]:
     # Load data
@@ -687,7 +697,7 @@ def load_data(args: Namespace, gpu_if_available: torch.device
         val_idxs = test_idxs
         near = 0
         far = depth_imgs.max() * 1.1
-        assert far < 72
+        assert far < 73
 
     else:
         raise RuntimeError(f'Unknown dataset type {args.dataset_type}. Exiting.')
@@ -710,14 +720,14 @@ def load_data(args: Namespace, gpu_if_available: torch.device
     intrinsics_matrix = torch.tensor(intrinsics_matrix)
 
     # Attempt to move data to the GPU.
-    rgb_imgs = torch.tensor(rgb_imgs, device=gpu_if_available, dtype=torch.float32)
-    depth_imgs = torch.tensor(depth_imgs, device=gpu_if_available, dtype=torch.float32)
-    poses = torch.tensor(poses, device=gpu_if_available, dtype=torch.float32)
-    render_poses = torch.tensor(render_poses, device=gpu_if_available, dtype=torch.float32)
+    rgb_imgs = torch.tensor(rgb_imgs, device=cpu, dtype=torch.float32)
+    depth_imgs = torch.tensor(depth_imgs, device=cpu, dtype=torch.float32)
+    poses = torch.tensor(poses, device=cpu, dtype=torch.float32)
+    render_poses = torch.tensor(render_poses, device=cpu, dtype=torch.float32)
 
-    print('TRAIN views are', train_idxs)
-    print('TEST views are', test_idxs)
-    print('VAL views are', val_idxs)
+    # print('TRAIN views are', train_idxs)
+    # print('TEST views are', test_idxs)
+    # print('VAL views are', val_idxs)
 
     train_idxs = torch.tensor(train_idxs)
     test_idxs = torch.tensor(test_idxs)
@@ -725,6 +735,32 @@ def load_data(args: Namespace, gpu_if_available: torch.device
 
     return (rgb_imgs, depth_imgs, hwf, img_height, img_width, focal, intrinsics_matrix, poses, render_poses, train_idxs, test_idxs,
             val_idxs, near, far)
+
+
+def load_data_and_create_keyframes(args, gpu_if_available, tensorboard, cpu):
+    # Load data from specified dataset.
+    (rgb_imgs, depth_imgs, hwf, img_height, img_width, focal, initial_intrinsics_matrix, initial_poses,
+     render_poses, train_idxs, _, val_idxs, near, far) = load_data(args, gpu_if_available, cpu)
+
+    val_idx = val_idxs[len(val_idxs) // 2]
+    val_rgb_img = rgb_imgs[val_idx]
+    val_depth_img = depth_imgs[val_idx]
+    val_pose = initial_poses[val_idx, :3, :4].to(gpu_if_available)
+
+    kf_rgb_imgs, kf_depth_imgs, kf_initial_poses, kf_idxs, n_kfs = \
+        create_keyframes(rgb_imgs, depth_imgs, initial_poses, train_idxs, args.keyframe_creation_strategy,
+                         args.every_Nth)
+
+    val_rgb_img = val_rgb_img.to(gpu_if_available)
+    val_depth_img = val_depth_img.to(gpu_if_available)
+    val_pose = val_pose.to(gpu_if_available)
+    kf_rgb_imgs = kf_rgb_imgs.to(gpu_if_available)
+    kf_depth_imgs = kf_depth_imgs.to(gpu_if_available)
+    kf_initial_poses = kf_initial_poses.to(gpu_if_available)
+
+    return (hwf, img_height, img_width, focal, initial_intrinsics_matrix, initial_poses, render_poses,
+            near, far, val_rgb_img, val_depth_img, val_pose, kf_rgb_imgs, kf_depth_imgs,
+            kf_initial_poses, kf_idxs, n_kfs)
 
 
 def split_into_sections(mats, grid_size):
@@ -989,8 +1025,8 @@ def sample_sw_rays(sw_rays: torch.Tensor, sw_sampling_prob_dist: torch.Tensor,
             # circle(sampling_vis_img_np, (pixel_col_idx, pixel_row_idx), 2, (1, 1, 1), FILLED)
             circle(sampling_vis_img_np, (pixel_col_idx, pixel_row_idx), 1, (1, 0, 0), FILLED)
 
-        tensorboard.add_images(tensorboard_tag, sampling_vis_imgs, train_iter_idx,
-                               dataformats='NHWC')
+        # tensorboard.add_images(tensorboard_tag, sampling_vis_imgs, train_iter_idx,
+        #                        dataformats='NHWC')
 
     t_delta = perf_counter() - t_start
     if verbose:
@@ -1255,7 +1291,7 @@ def create_keyframes(rgb_imgs: torch.Tensor, depth_imgs, initial_poses: torch.Te
     kf_initial_poses = initial_poses[kf_idxs]
     n_kfs = len(kf_idxs)
 
-    print('Keyframe views are ', kf_idxs)
+    print('Number of keyframes:', kf_idxs.shape[0])
 
     return kf_rgb_imgs, kf_depth_imgs, kf_initial_poses, kf_idxs, n_kfs
 
@@ -1328,6 +1364,8 @@ def initialize_sw_kf_loss(kf_rgb_imgs, kf_depth_imgs, kf_poses, sw_unif_sampling
 
     with torch.no_grad():
         # Get all rays from all keyframes.
+        # return torch.ones(dims_kf_sw, dtype=torch.float32, device=cpu)
+
         sw_kf_rays, _ = get_sw_rays(kf_rgb_imgs, kf_depth_imgs, img_height, img_width, intrinsics_matrix, kf_poses,
                                     n_kfs, grid_size, section_height, section_width, gpu_if_available)
     # Uniformly sample rays across all keyframes.
@@ -1681,7 +1719,11 @@ def get_depth_loss_meters_multiplier(gt_depth):
     shift = torch.log(torch.tensor(1 / height)) / steepness - center
 
     depth_loss_meters_multiplier = \
-        multiplier_min + 1 / (1 / height + torch.exp(steepness * (gt_depth + shift)))
+        multiplier_min + 1 / (1 / height + torch.exp((steepness * (gt_depth +
+                                                                   shift)).clamp(max=50)))
+
+    # if gt_depth.requires_grad:
+    #     set_trace()
 
     return depth_loss_meters_multiplier
 